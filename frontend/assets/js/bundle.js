8fc21372f7065f9575a342af21e859ec2e28af3c
igor <igor.siquieri@devapi.com.br>
2023-08-15 17:24:34 -0300

    init

---
+++ frontend/assets/js/bundle.js
@@ -0,0 +1,61 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/A0018-webpack/form-control.ts":
+/*!*******************************************!*\
+  !*** ./src/A0018-webpack/form-control.ts ***!
+  \*******************************************/
+/***/ (() => {
+
+
+const SHOW_ERROR_MESSAGE = 'show-error-message';
+const form = document.querySelector('.form');
+const username = document.querySelector('.form');
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+/*!************************************!*\
+  !*** ./src/A0018-webpack/index.ts ***!
+  \************************************/
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+__webpack_require__(/*! ./form-control */ "./src/A0018-webpack/form-control.ts");
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
